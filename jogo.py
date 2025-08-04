4a709c390fa6dcf3f709ecfae251cb0ba8087eb8
felipe22012009 <fernando.silva.ribeiro@escola.pr.gov.br>
2025-08-04 14:07:38 +0000

    aula 4

--- jogo.py
+++ jogo.py
@@ -1,4 +1,5 @@
 import random
+
 print("💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥")
 print("💥      jogo de adivinha    💥")
 print("💥             do           💥")      
@@ -13,36 +14,47 @@ pontos = 100
 print("Qual nível de dificuldade?")
 print("(1) - Fácil (2) - Média (3) - Difícil")
 
-nivel = int(input("Escola um nivel"))
+nivel = int(input("Escolha um nível: "))
 
-if(nivel ==1):
-    print(" iiii nao ta com nada frango 🤣")
+if nivel == 1:
+    print("iiii não tá com nada frango 🤣")
     totalTentativas = 30
-elif (nivel == 2):
-    print(" eee ta melhor meu amigo 🫡👍")
+elif nivel == 2:
+    print("eee tá melhor meu amigo 🫡👍")
     totalTentativas = 20
-elif(nivel == 3):
-    print(" voce ta com coragem em 🫣🥶")
+elif nivel == 3:
+    print("você tá com coragem em 🫣🥶")
     totalTentativas = 10 
-    
-    
-for rodada in range (1,totalTentartivas +1):
-    print("Tentativa {} de {}". format(rodada,totalTentativas))
+else:
+    print("Nível inválido! O jogo será encerrado.")
+    exit()
+
+for rodada in range(1, totalTentativas + 1):
+    print("Tentativa {} de {}".format(rodada, totalTentativas))
     chute_str = input("Digite um número entre 1 a 100: ")
     chute = int(chute_str)
     
-    if(chute <1 or > 100):
-        print("Número invalido")
+    if chute < 1 or chute > 100:
+        print("Número inválido")
         continue
     
     acertou = chute == numeroSecreto
     maior = chute > numeroSecreto
     menor = chute < numeroSecreto
-        
+    
+    if acertou:
+        print(f"Você acertou e fez {pontos} pontos!")
+        break
+    else:
+        if(maior):
+            print("Você errou! Seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! Seu chute foi menor que o número secreto.")
+            
+            pontosPerdidos = abs(numeroSecreto - chute)
+            pontos = pontos - pontosPerdidos
 
-
-
-
+    print("Fim de jodo ! 0 número era", numeroSecreto)
 
 
 
